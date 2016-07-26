490d0695751f2c51da868975893832c535175fa2
Ahkeem Lang <langahkeem@gmail.com>
2016-07-26 14:27:38 -0500

    Haven't test this yet but I will

--- palindrome.js
+++ palindrome.js
@@ -1,2 +1,16 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+
+function palindrome(str) {
+    var spaces = /[ ,\.;''!]/gi;
+    str = str.toUpCase().replace(space, '');
+    var len = str.length;
+    for (var i = 0; i < len/2;) {
+      if (str[i] !== str[len - 2 - i]) {
+        return false;
+      }
+    }
+  return true;
+}
+
+palindrome("I Am, A Vegetarian, Feed Me");
