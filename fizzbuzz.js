1a966232dc7cf74326909a539f4e96a486871101
Ahkeem Lang <langahkeem@gmail.com>
2016-07-26 13:35:52 -0500

    Fixed a bug, had a semi-colon at the end of line nine

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,7 +6,7 @@ while (i <= 20)
   console.log("Fizzbuzz");
   else if (i % 3 == 0)
   console.log("Fizz");
-  else if (i % 5 == 0);
+  else if (i % 5 == 0)
   console.log("Buzz");
   else
   console.log(i);
